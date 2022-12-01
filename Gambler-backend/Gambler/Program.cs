089d04ec30d8cbd5776f3b21e0d22026c3fe21fb
Joar Langaard <joar.langaard@visma.com>
2022-12-01 13:03:23 +0100

    Added some cool stuff to the program.cs

--- Gambler-backend/Gambler/Program.cs
+++ Gambler-backend/Gambler/Program.cs
@@ -5,16 +5,13 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services to the shipping container. Please contact your freight shipping services if you have any issues
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(options => 
-    options.UseSqlServer("Server=.\\SQLExpress;Database=GamblerDb;Trusted_Connection=true;;TrustServerCertificate=True"));
+    options.UseSqlServer("Server=.\\SQLExpress;Database=GamblerDb;User Id=sa;Password=letmein;TrustServerCertificate=True"));
 
-//builder.Services.AddScoped<ISuperHeroRepository, SuperHeroRepository>();
 builder.Services.AddScoped<IGamblerService, GamblerService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
@@ -27,20 +24,18 @@ builder.Services.AddCors(options =>
         });
     });
 
+// Bob The Builder can you fix it!
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
-// Nah, don't use it!
+// Nah, don't use it! We use decurity through obscurity
 //app.UseMiddleware<ApiKeyMiddleware>();
 
 app.UseCors("MyAllowAllCORSPolicy");
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
-app.Run();
+app.Run(); // forrest, run!
