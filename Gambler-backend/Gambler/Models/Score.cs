6f9c0eeb96002874523f1a04113587e421b7f1bd
Joar Langaard <joar.langaard@visma.com>
2022-12-03 11:29:51 +0100

    More info in the score entity

--- Gambler-backend/Gambler/Models/Score.cs
+++ Gambler-backend/Gambler/Models/Score.cs
@@ -5,5 +5,8 @@
         public string Nickname { get; set; } = string.Empty;
         public int Points { get; set; }
         public string Message { get; set; } = string.Empty;
+        public DateTime LatestBet { get; set; }
+        public int Highscore { get; set; }
+        public int NumberOfBets { get; set; }
     }
 }
